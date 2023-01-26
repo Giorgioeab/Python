b24e6f5ce59108505f677bc81ca7d6ce4517663a
Giorgio Almeida <eltoncr7@hotmail.com>
2023-01-26 01:58:54 -0300

    Atualização das funções

--- CRUD.py
+++ CRUD.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import pandas as pd
 
+
 # Conexão com servidor (atualmente local)
 conexao = mysql.connector.connect(
     host='127.0.0.1',
@@ -14,8 +15,9 @@ cursor = conexao.cursor()
 
 def inserir():
     novo_aluno = input('Digite o nome do aluno: ')
+    cpf_aluno = int(input('Digite o CPF do aluno (APENAS NUMEROS)'))
     novo_curso = input('Digite o curso do aluno: ')
-    comandoSQL = f'INSERT INTO aluno (nome_aluno, curso_aluno) values ("{novo_aluno}","{novo_curso}")'
+    comandoSQL = f'INSERT INTO aluno (nome_aluno, cpf_aluno, curso_aluno) values ("{novo_aluno}",{cpf_aluno},"{novo_curso}")'
     cursor.execute(comandoSQL)
     conexao.commit()
 
@@ -25,7 +27,7 @@ def consultarTodos():
     cursor.execute(comandoSQL)
     resultadoDaConsulta = cursor.fetchall()
     df = pd.DataFrame(resultadoDaConsulta)
-    print(df)
+    print(f'{df} \n')
 
 
 def consultaAlunoPorID():
@@ -34,19 +36,22 @@ def consultaAlunoPorID():
     cursor.execute(comandoSQL)
     resultadoDaConsulta = cursor.fetchall()
     df = pd.DataFrame(resultadoDaConsulta)
-    print(df)
+    print(f'{df} \n')
+
 
 def atualizar():
-    atualizaAluno = input('Atualize para o nome: ')
     idAluno = int(input('Digite o ID do Aluno a ser atualizado: '))
-    comandoSQL = f'UPDATE aluno set nome_aluno="{atualizaAluno}" where id_aluno={idAluno}'
+    atualiza_aluno = input('Atualize para o nome: ')
+    atualiza_cpf = int(input('Digite o cpf: '))
+    atualiza_curso = input('Digite o curso do aluno: \n')
+    comandoSQL = f'UPDATE aluno set nome_aluno="{atualiza_aluno}", cpf_aluno={atualiza_cpf}, nome_curso="{atualiza_curso}" where id_aluno={idAluno}'
     cursor.execute(comandoSQL)
     conexao.commit()
 
 
 def delete():
     idAluno = int(input('Digite o ID do Aluno a ser excluido: '))
-    comandoSQL = f'DELETE FROM aluno where id_aluno = {idAluno}'
+    comandoSQL = f'DELETE FROM aluno where id_aluno = {idAluno}\n'
     cursor.execute(comandoSQL)
     conexao.commit()
 
@@ -62,16 +67,22 @@ while True:
     match menu:
         case 1:
             inserir()
+            print('-' *50)
         case 2:
             consultarTodos()
+            print('-' *50)
         case 3:
             consultaAlunoPorID()
+            print('-' *50)
         case 4:
             atualizar()
+            print('-' *50)
         case 5:
             delete()
+            print('-' *50)
         case 6:
             print('Você saiu com Sucesso!')
+            print('-' * 50)
             break
         case _:
             print('Opção Inválida!')
